9a907c417f85b013e0d3f47199e33d797f42347e
Brianna Greene <brianna.greene.designs@gmail.com>
2017-11-15 11:44:47 -0700

    added react front end

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,10 +1,10 @@
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const TwitterStrategy = require('passport-twitter').Strategy
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 const keys = require('../config/keys')
 
-// const User = mongoose.model('users')
+const User = mongoose.model('users')
 
 passport.use(
   new GoogleStrategy(
@@ -14,28 +14,29 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      // User.findOne({ googleId: profile.id }).then(existingUser => {
-      //   if (existingUser) {
-      //     done(null, existingUser)
-      //   } else {
-      //     new User({ googleId: profile.id })
-      //       .save()
-      //       .then(user => done(null, user))
-      //   }
-      // })
+      console.log(accessToken);
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser)
+        } else {
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user))
+        }
+      })
     }
   )
 )
 
-passport.use(new TwitterStrategy({
-    consumerKey: keys.twitterConsumerKey,
-    consumerSecret: keys.twitterConsumerSecret,
-    callbackURL: "/auth/twitter/callback"
-  },
-  function(token, tokenSecret, profile, cb) {
-    console.log('token from twitter:', token);
-    // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
-    //   return cb(err, user);
-    // });
-  }
-));
+// passport.use(new TwitterStrategy({
+//     consumerKey: keys.twitterConsumerKey,
+//     consumerSecret: keys.twitterConsumerSecret,
+//     callbackURL: "/auth/twitter/callback"
+//   },
+//   function(token, tokenSecret, profile, cb) {
+//     console.log('token from twitter:', token);
+//     // User.findOrCreate({ twitterId: profile.id }, function (err, user) {
+//     //   return cb(err, user);
+//     // });
+//   }
+// ));
